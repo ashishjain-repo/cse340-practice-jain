cfcf3b696550b321d228588c3d86228ccb245ddf
Ashish Jain <ashishjain99@yahoo.com>
2025-01-18 13:26:07 -0700

    Added Templates

--- server.js
+++ server.js
@@ -7,23 +7,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-
 const NAME = process.env.NAME;
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-});
-app.get('/page1', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/page1.html'));
-});
-app.get('/page2', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/page2.html'));
-});
-
-app.get('/about-us', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/about-us.html'))
+    const title = 'Home Page';
+    const content = '<h1>Welcome to the Home Page</h1>';
+    res.render('index', {title, content});
 });
 
 const PORT = 3000;
